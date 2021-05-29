53d1995f5edf55aebafbb4329e86eb5b0c971bcf
Darbu <kostas.92@mail.ru>
2021-05-29 22:55:37 +0300

    exmpl2

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace WpfApp1
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Hello");
+            MessageBox.Show("Hello1");
         }
     }
 }
